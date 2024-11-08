3411bcf0184be4d712fe2e253452d4fec03f9f71
Daniel Rego Calvo <danregcal@gmail.com>
2024-11-08 22:02:35 +0100

    Changed number of memcache nodes

--- Memcached.tf
+++ Memcached.tf
@@ -7,7 +7,8 @@ module "memcached" {
 
   engine               = "memcached"
   node_type            = "cache.t4g.micro" 
-  num_cache_nodes      = 1                   
+  num_cache_nodes      = 2                  
+  az_mode              = "cross-az"
   parameter_group_name = "default.memcached1.6" 
 
   # Security group
